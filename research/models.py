73dfdbde789530fe740c815fafb6b722cdb2f5a7
farhadfreestyle <agayevferhad02@gmail.com>
2023-12-31 00:37:47 +0400

    model memory error done

--- research/models.py
+++ research/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Research(models.Model):
-    title = models.CharField(max_length = 50)
+    title = models.CharField(max_length = 500)
     description = models.TextField()
     link = models.URLField()
    
