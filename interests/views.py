150638091e19ff3b33dc9b46d1ffa2544fe6ff13
farhadfreestyle <agayevferhad02@gmail.com>
2023-12-31 23:10:31 +0400

    interests listed

--- interests/views.py
+++ interests/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from interests.models import Interest
 # Create your views here.
 def interests(request):
-    interests = Interest.objects.all().order_by('-interest_name')
+    interests = Interest.objects.all().order_by('interest_name')
 
     return render(request, 'interests/interests.html', {'interests':interests})
